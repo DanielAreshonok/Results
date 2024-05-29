cdadca9b493ee19fad3ca9e0f309987af5ca6d8c
DanielAreshonok <165793831+DanielAreshonok@users.noreply.github.com>
2024-05-30 00:51:15 +0300

    Добавлен вывод общей информации о задании

--- git/git/main.cpp
+++ git/git/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+
 using namespace std;
 
 int main()
 {
-    std::cout << "Hello\n";
+    cout << "Laboratory work #10. GIT\n";
+    cout << "Variant #1. Marathon Results\n";
+    cout << "Author: Daniel Areshonok\n";
     return 0;
 }
