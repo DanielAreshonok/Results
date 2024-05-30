0a1c27c8b8bfc3601a9c24ebf74562747525e29d
Daniel <danielareshonok@gmail.com>
2024-05-30 10:56:07 +0300

    новый commit

--- git/git/main.cpp
+++ git/git/main.cpp
@@ -7,5 +7,6 @@ int main()
     cout << "Laboratory work #10. GIT\n";
     cout << "Variant #1. Marathon Results\n";
     cout << "Author: Daniel Areshonok\n";
+    cout << "Group: 23IRM1D\n";
     return 0;
 }
