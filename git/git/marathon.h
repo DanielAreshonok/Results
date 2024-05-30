865b9e9928cbfb3bf76e29cdb70dba7f4f193403
Daniel <danielareshonok@gmail.com>
2024-05-30 11:38:01 +0300

    Добавлены структуры для хранения данных

---
+++ git/git/marathon.h
@@ -0,0 +1,29 @@
+#ifndef BOOK_SUBSCRIPTION_H
+#define BOOK_SUBSCRIPTION_H
+
+#include "constants.h"
+
+struct time
+{
+    int min;
+    int sec;
+    int milsec;
+};
+
+struct person
+{
+    char first_name[MAX_STRING_SIZE];
+    char middle_name[MAX_STRING_SIZE];
+    char last_name[MAX_STRING_SIZE];
+};
+
+struct results
+{
+    int number;
+    person competitor;
+    time start;
+    time finish;
+    char club[MAX_STRING_SIZE];
+};
+
+#endif
